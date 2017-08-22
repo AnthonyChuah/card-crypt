8386d65a366f5a1bff63bc01f2c58a4a7f203d97
Anton Chuah <anthony.w.chuah@gmail.com>
2017-08-22 12:27:56 +0100

    Some boilerplate and logic written in Cipher.cpp

--- Cipher.cpp
+++ Cipher.cpp
@@ -5,6 +5,7 @@
 
 Cipher::Cipher() {
   key_.reserve(this->NCARDS);
+  keystream_.reserve(this->NCARDS);
 }
 
 std::vector<int> Cipher::generateKey() {
@@ -22,7 +23,7 @@ std::vector<int> Cipher::generateKeystream(int _n) {
      2. Find JOKER-B (54). Move it down by two steps (swap with adjacent-below twice)
      Note: if "below" is the end of the deck, treat the deck as circular (wrap back to top)
      3. Perform the Triple Cut: swap all cards above higher JOKER with all cards below lower JOKER
-     4. Perform the Count Cut: get bottom card (54th card). That gives an index.
+     4. Perform the Count Cut: get bottom card (54th card). That gives an index. Either joker is 53, not 54.
         Now search for the card in that position (1-indexed, adjust to 0-indexing when programming) in deck.
         Now all cards up to and including that card, treat it as a slice.
         All cards that are not in that slice, but not including the bottom card (which provided the index!)
@@ -32,8 +33,45 @@ std::vector<int> Cipher::generateKeystream(int _n) {
         Using 1-indexing, find the card which the index points to. That card's value is the output!
 	If that card is a JOKER, do nothing and start all over again from step 1.
    */
-  int count = 0;
+  std::vector<int> output(_n);
+  int count = 0; int pos; std::pair<int, int> jokers;
   while (count < _n) {
-    
+    pos = this->findJokerA();
+    this->swapCards(pos, pos + 1);
+    pos = this->findJokerB();
+    this->swapCards(pos, pos + 1);
+    this->swapCards(pos + 1, pos + 2);
+    jokers = this->findJokers();
+    this->tripleCut(jokers.first, joker.second);
+    pos = key_[this->NCARDS - 1];
+    this->countCut(pos); // Handle the pos == 54 exception inside helper method
+    keystream_[count] = this->findOutput(); // Check if std::vector<int> output(_n) was initialized with len 54!
+    if (keystream_[count] < 0) {
+      continue;
+    } else {
+      ++count;
+    }
   }
 }
+
+std::string Cipher::encrypt(std::string _plaintext) {
+  std::vector<int> textInInts = Cipher::convertCharsToInts(_plaintext);
+  int nChars = _plaintext.length();
+  key_ = initKey_;
+  generateKeystream(nChars);
+  // Now add then mod 26
+  std::vector<int> ctextInInts(nChars);
+  // Translate back to text form by iterating over vector
+}
+
+std::string Cipher::decrypt(std::string _ciphertext) {
+  std::vector<int> ctextInInts = Cipher::convertCharsToInts(_ciphertext);
+  int nChars = _ciphertext.length();
+  key_ = initKey_;
+  generateKeystream(nChars);
+  // Now subtract then mod 26. Remember negatives should wrap around
+  std::vector<int> textInInts(nChars);
+  // Translate back to text form by iterating over vector
+}
+
+// Private helper methods
